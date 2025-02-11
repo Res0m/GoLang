e1d50fb1b743e28047332e3eb933ebe0da8363a5
Anton Remzov <ant.remzov@yandex.ru>
2025-02-11 20:09:45 +0300

    11.02.2025

---
+++ 2-nd project/go.mod
@@ -0,0 +1,3 @@
+module golang/app-2
+
+go 1.23.6
--- 2-nd project/main2.go
+++ 2-nd project/main2.go
@@ -1,10 +1,23 @@
 package main 
 
-import(
+import (
 	"fmt"
 )
 
+
 func main(){
-	fmt.Println("qq")
+	transactions := []int{0, 20, 35} // [] - длина + тип (например float) + {}содержимое масива (например: {1, 2, 4})
+	temp := transactions
+
+	// partial := transactions[1:] // [1:n] - срез масива (от 1 до n)	
+	transactions = append(transactions, 100)
+	fmt.Println(temp)
+	fmt.Println(transactions)
+
+	
+
+
+
+
 
 }
