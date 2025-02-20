07334afd3ad2eb57b71cc29ca62bb9e5cc566ebd
Anton Remzov <ant.remzov@yandex.ru>
2025-02-20 21:34:53 +0300

    Structure practice 20.02.2025

--- 4-nd project/main.go
+++ 4-nd project/main.go
@@ -1,16 +1,69 @@
 package main
 
-import "fmt"
 
-func main() {
-	a := [4]int{1, 2, 3, 4}
-	reverse(&a) // Меняет порядок элементов на обратный
-	fmt.Println(a)
+
+
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+
+
+
+
+
+type account struct {
+	login string
+	password string
+	url string
 }
 
-func reverse(array *[4]int){
-	for index, value := range *array{
-		(*array)[len(*array) - 1 - index] = value
-
+func main() {
+	fmt.Println(rand.IntN(10))
+	str :=[]int32("Привет!)")
+	for _, ch := range string(str){
+		fmt.Println(ch, string(ch))
 	}
+
+
+
+
+
+
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL")
+
+	myAccount := account{
+		login: login,
+		password: password,
+		url: url,
+	}
+	// }
+	// account2 := account{
+	// 	login: login,
+	// 	password: password,
+	// }
+	// account3 := account{}
+	outputPassword(&myAccount)
+}
+
+
+func promptData(prompt string) string { 
+	fmt.Print(prompt, ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+
+func outputPassword(acc *account){
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+
+func generatePassword(n int) string{
+	
 }
